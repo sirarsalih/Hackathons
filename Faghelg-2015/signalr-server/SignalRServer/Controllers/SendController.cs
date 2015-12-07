1cdcb2912b4ccd5854f1ff9d45291607e83f1996
sirar87 <sirars@gmail.com>
2015-12-07 23:00:08 +0100

    Derotate image if it was rotated wrongly. Remove aspect ratio constraint.

--- Faghelg-2015/signalr-server/SignalRServer/Controllers/SendController.cs
+++ Faghelg-2015/signalr-server/SignalRServer/Controllers/SendController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Configuration;
+using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 using Microsoft.AspNet.SignalR;
 using Microsoft.ServiceBus;
@@ -50,9 +52,43 @@ namespace SignalRServer.Controllers
         public HttpStatusCodeResult SendImage()
         {
             var imageFile = Request.Files[0];
-            MemoryStream target = new MemoryStream();
-            imageFile.InputStream.CopyTo(target);
-            byte[] imageBytes = target.ToArray();
+
+            var ms = new MemoryStream();
+            imageFile.InputStream.CopyTo(ms);
+            var image = Image.FromStream(ms);
+
+            if (image.PropertyIdList.Contains(0x0112))
+            {
+                int rotationValue = image.GetPropertyItem(0x0112).Value[0];
+                switch (rotationValue)
+                {
+                    case 1: // landscape, do nothing
+                        break;
+
+                    case 8: // rotated 90 right
+                        // de-rotate:
+                        image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        break;
+
+                    case 3: // bottoms up
+                        // de-rotate:
+                        image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+
+                    case 6: // rotated 90 left
+                        // de-rotate:
+                        image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                }
+            }
+
+            byte[] imageBytes;
+            using (ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                imageBytes = ms.ToArray();
+            }
+            
             var imageContent = new ImageContent()
             {
                 ImageDateTime = DateTime.Now,
@@ -77,6 +113,11 @@ namespace SignalRServer.Controllers
             }
 
             return File(imageContent.ImageData, System.Net.Mime.MediaTypeNames.Image.Jpeg);
+        }
+
+        private static void DerotateIfRotatedWrongly(Image image)
+        {
+           
         }
     }
 }
